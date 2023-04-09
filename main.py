2ebb307c0c9b20d6cf65076c695832fcdfccc6f6
Bret Montgomery <example@email.com>
2023-04-09 12:02:26 -0700

    Moved strings to multiple lines

--- main.py
+++ main.py
@@ -15,11 +15,14 @@ if uploaded_file is not None:
   df.COST = df.COST.str.replace('$', '', regex=False).astype('float16')
 
   st.subheader('Now we can view the contents')
-  st.write('We are simply making sure that everything looks properly formatted before we aggregate the data')
+  st.write("We are simply making sure that everything looks properly formatted "
+           "before we aggregate the data")
   st.dataframe(df)
 
   st.subheader('Finally, we sum the usage and cost')
-  st.write("We now aggregate the usage and costs fields using a simple sum. Keep in mind that the first of the month is shown even though this is the entire month's usage")
+  st.write("We now aggregate the usage and costs fields using a simple sum. "
+           "Keep in mind that the first of the month is shown even though this "
+           "is the entire month's usage")
   usage = df.resample('MS').sum(numeric_only=True)
   usage.index = usage.index.date
   st.dataframe(usage.style.format(precision=2))
